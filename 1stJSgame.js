0e04d9cc719ca19dcd7ee9feae07b35c4d412326
iggirex <iggirex@gmail.com>
2016-06-10 15:23:03 -0600

    Create 1stJSgame.js

---
+++ 1stJSgame.js
@@ -0,0 +1,9 @@
+alert("Why hello there!" "\n" "The Earth has just exploded. Everyone is dead except you and the mutant monsters!" "\n" "Congratulations sURVIVOR!!");
+
+var answer1 = prompt("how old are you?");
+
+if(answer1 <55){
+alert("The mutants like old flesh best" "\n" "And so you're dead!!");
+} else {
+alert("Good! Mutants LOVE old flesh, so the older you get, the more they'll hunt you down" "\n" "Great news youngin!!");
+}
