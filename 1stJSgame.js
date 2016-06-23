8491a04976e809e18d945eed6d839f840d624c05
Ignacio (iggi) Aguerrevere Bazo <iggirex@gmail.com>
2016-06-22 18:05:21 -0600

    Update 1stJSgame.js

--- 1stJSgame.js
+++ 1stJSgame.js
@@ -1,9 +1,11 @@
 alert("Why hello there!" "\n" "The Earth has just exploded. Everyone is dead except you and the mutant monsters!" "\n" "Congratulations sURVIVOR!!");
 
-var answer1 = prompt("how old are you?");
+var answer1 = prompt("What is your preferred mutant fighting weapon?<br/> A)Stick B)Rock");
 
-if(answer1 <55){
-alert("The mutants like old flesh best" "\n" "And so you're dead!!");
+if(answer1 == "Stick" ){
+alert("This Stick is strong and has good reach" "\n" "It will come in handy!!");
+} else if ( answer1 == "Rock") {
+alert("This is good for throwing" "\n" "Better take aim!!");
 } else {
-alert("Good! Mutants LOVE old flesh, so the older you get, the more they'll hunt you down" "\n" "Great news youngin!!");
+  alert("That is not a valid weapon")
 }
